909e005b005860f28f3133315219b2b4a81b9144
Malini Janaki Sankaran <janakisankaran.m@northeastern.edu>
2024-10-31 14:24:58 -0400

    CHanges for Image schema

---
+++ service/models/Image.js
@@ -0,0 +1,54 @@
+// models/Image.js
+import  {Sequelize, DataTypes } from 'sequelize';
+import  {sequelize}  from '../services/health-service.js';
+
+const Image = sequelize.define('Image', {
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+        allowNull: false,
+        readOnly: true, 
+    },
+    file_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    url: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        readOnly: true, 
+    },
+    upload_date: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+        allowNull: false,
+        readOnly: true, 
+    },
+    user_id: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        unique: true,
+        references: {
+            model: 'Users', 
+            key: 'uuid',
+        },
+        onDelete: 'CASCADE',
+        readOnly: true, 
+    },
+}, {
+    timestamps: false,
+});
+
+
+
+// Sync the Image model to create the table
+Image.sync()
+    .then(() => {
+        console.log('Image table created successfully.');
+    })
+    .catch((error) => {
+        console.error('Error creating Image table:', error);
+    });
+
+export default Image;
