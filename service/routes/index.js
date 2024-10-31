9e1176f3a3f2835e147008d6496bd2d7124ff464
Malini Janaki Sankaran <janakisankaran.m@northeastern.edu>
2024-10-31 14:27:26 -0400

    File changes for server.js and package.json

--- service/routes/index.js
+++ service/routes/index.js
@@ -1,6 +1,6 @@
 import healthRouter from './health-route.js'; // To import health check routes
 import userRoutes from './user-routes.js';// To import the user routes
-import imageRoutes from './s3image-route.js'; // To import the image routes
+
 
 const initializeRoutes =(app)=>{
     app.use('/healthz', healthRouter); //Using the health check router
