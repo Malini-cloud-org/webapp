d0c8b859990c5f102517208f88329339f8c4f9b1
Malini240 <janakisankaran.m@northeastern.edu>
2024-12-06 16:49:28 -0500

    Update index.js

--- service/routes/index.js
+++ service/routes/index.js
@@ -6,6 +6,8 @@ import userRoutes from './user-routes.js';// To import the user routes
 const initializeRoutes =(app)=>{
     app.use('/healthz', healthRouter); //Using the health check router
     app.use('/v1/user', userRoutes); //Using the user routes 
+
+    app.use('/cicd', healthRouter);
     
 };
 
